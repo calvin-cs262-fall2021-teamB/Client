08965f6116bc20c3f0ecaa5140ac191c7037206b
unknown <mindysport@gmail.com>
2021-10-13 23:04:26 -0400

    fixing login screen and adding navigation

--- styles/global.js
+++ styles/global.js
@@ -5,6 +5,7 @@ export const globalStyles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
+    //backgroundColor: '#B0ADAD',
     backgroundColor: '#E5E5E5',
   },
   OrdersWrapper: {
@@ -54,5 +55,77 @@ export const globalStyles = StyleSheet.create({
   chooseOrderText: {
     textAlign: 'center',
     color: 'white',
-  }
+  },
+  //header styles
+  loginContainer:{
+    flex: 1,
+    padding: 2,
+  },
+  tinyLogo:{
+    width: 135,
+    height: 65
+  },
+  //login styles
+  largeLogo:{
+    height: 275,
+    width: 275, 
+    alignSelf: 'center', 
+    borderRadius: 200,
+    marginTop: '10%'
+  },
+  loginWrapper:{
+    justifyContent: "flex-end",
+  },
+  keyboardContainer:{
+    justifyContent: "flex-end",
+  },
+  login:{
+    marginTop: 40,
+    marginBottom: 5,
+    // marginLeft: 20,
+    fontSize: 35,
+    // fontWeight: 'bold',
+    color: '#333',
+  },
+  subLogin:{
+      marginLeft: 10,
+      marginBottom: 10,
+      fontSize: 18,
+      color: '#333',
+  },
+  loginMessage:{
+      marginLeft: 10,
+      marginBottom: 15,
+      fontSize: 18,
+      color: '#333',
+  },  
+  text:{
+      fontSize: 18,
+      alignSelf: 'center',
+      fontWeight: 'bold',
+      color: 'white',
+  },
+  buttonContainer:{
+      // marginTop: 5,
+      elevation: 8,
+      backgroundColor: "#800000",
+      borderRadius: 10,
+      // paddingVertical: 1,
+      // paddingHorizontal: 12
+  },
+  submitContainer:{
+    flex: 1,
+    padding: 10,
+    marginTop: 15,
+    marginBottom: 15, 
+    justifyContent: 'center'
+  },
+  safeContainer: {
+    flex: 1,
+  },
+  inner: {
+    padding: 10,
+    flex: 1,
+    justifyContent: "flex-end",
+  },
 });
